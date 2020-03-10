173018d70e577aa18f4ce824841e821c8056a554
Milosz Gawel <mgawel@gmail.com>
2020-03-11 00:52:14 +0100

    portfolio section fixed

--- scripts/masonry.js
+++ scripts/masonry.js
@@ -1,13 +1,16 @@
 $(function() {
   $("#WS-LiSli").WS_lightbox_free({
     enable: ["close", "arrows"],
+    close: {
+      size: 4000
+    },
     buttons: {
-      size: 60,
+      size: 40,
       style: "square", // ( default, square, circle, rounded )
       color: "black",
       color_icon: "white",
-      border_width: 0,
-      border_color: "",
+      border_width: 1,
+      border_color: "white",
       opacity: 0.5,
       hover_opacity: 1
     },
@@ -17,11 +20,11 @@ $(function() {
       icon_style: "angle", // (default, caret, angle, chevron)
       icon_size: 60,
       opacity: 0.8,
-      hover_effect: "zoom", // (zoom, shrink, translate)
+      hover_effect: "shrink", // (zoom, shrink, translate)
       hover_opacity: 0.8
     },
     image: {
-      border_radius: 0,
+      border_radius: 50,
       border_color: "",
       border_width: 0,
       bg: "none"
